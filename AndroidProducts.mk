97e331f17459da5120ce0d261d2d31f1141996bc
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2023-08-02 19:50:33 +0200

    TECNO-CK7n: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: arm64 Manufacturer: tecno Model: TECNO CK7n Script version: 3.0.0

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2023 The Android Open Source Project
+# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_TECNO-CK7n.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_TECNO-CK7n-user \
+    omni_TECNO-CK7n-userdebug \
+    omni_TECNO-CK7n-eng
